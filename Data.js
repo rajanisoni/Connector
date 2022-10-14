3a437bcd1d46191a568f207c4a1d473a7db241b2
Rajani Soni <rajnisoni04.002@gmail.com>
2022-10-14 20:20:02 +0530

    Added static data

---
+++ Data.js
@@ -0,0 +1,111 @@
+
+
+ export const posts = [
+    {
+        name: "Per Borgen",
+        img: "images/Per.jpg",
+        linkedin: "https://www.linkedin.com/in/per-harald-borgen/",
+        github: "https://github.com/perborgen",
+        twitter: "https://twitter.com/perborgen"
+
+    },
+
+    {
+        name: "Guil Hernandez",
+        img: "images/Guil.webp",
+        linkedin: "https://www.linkedin.com/in/guiljh/",
+        twitter: "https://twitter.com/guilh?lang=en",
+        github: "https://github.com/Guilh"
+       
+
+    },
+
+    {
+        name: "Bob Ziroll",
+        img: "images/Bob.png",
+        linkedin: "https://www.linkedin.com/in/bobziroll/",
+        twitter: "https://twitter.com/bobziroll",
+        github: "https://github.com/bobziroll"
+      
+
+    },
+
+    {
+        name: " Kevin Powell",
+        img: "images/Kevin.jpg ",
+        linkedin: "https://www.linkedin.com/kevin-powell ",
+        twitter: "https://twitter.com/kevinjpowell ",
+        github: " https://github.com/kevin-powell"
+        
+
+    },
+
+    {
+        name: "Leanne Rybintsev 🐱 ",
+        img: "images/leanne.webp ",
+        linkedin: "https://www.linkedin.com/in/leannerybintsev/ ",
+        twitter: "https://twitter.com/rybaleanne ",
+        github: "https://github.com/lkrryba "
+        
+
+    },
+
+    {
+        name: "Ania Kubow ",
+        img: "images/Ania.png ",
+        linkedin: "https://www.linkedin.com/in/ania-kubow/ ",
+        twitter: " https://twitter.com/ania_kubow?lang=gl",
+        github: "https://www.youtube.com/aniakubow "
+        
+
+    },
+
+    {
+        name: "Gary Simon ",
+        img: "images/Gary.png ",
+        linkedin: "https://www.linkedin.com/in/dreamache/ ",
+        twitter: " https://twitter.com/designcoursecom",
+        github: "https://www.youtube.com/c/DesignCourse/featured "
+        
+    },
+    
+    {
+        name: "Dylan C. Israel ",
+        img: "images/dylan.jpg ",
+        linkedin: "https://www.linkedin.com/in/dylan310/ ",
+        twitter: "https://twitter.com/PizzaPokerGuy ",
+        github: " https://github.com/Dylan-Israel"
+        
+    },
+
+    {
+        name: " Cassidy Williams",
+        img: "images/Cassidy.jpg ",
+        linkedin: " https://www.linkedin.com/in/cassidoo/",
+        twitter: " https://twitter.com/cassidoo",
+        github: " https://github.com/cassidoo"
+        
+    },
+
+    {
+        name: "Thomas Chant",
+        img: "images/tom.webp ",
+        linkedin: " https://www.linkedin.com/in/thomaspchant/",
+        twitter: "https://twitter.com/tpchant ",
+        github: " https://www.youtube.com/watch?v=pfmT3DLdB78&ab_channel=Scrimba"
+        
+    },
+
+    // {
+    //     name: " ",
+    //     img: "images/ ",
+    //     linkedin: " ",
+    //     twitter: " ",
+    //     github: " "
+        
+    // }
+]
+
+
+
+
